4cc2923a24b16700e5e1bbe62c72326000541ea7
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2019-08-05 02:05:30 -0500

    Added DotNetMSBuild

---
+++ src/Nuke.MsBuild/DotNetMSBuild.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.IO.FileSystemTasks;
+
+namespace Rocket.Surgery.Nuke.MsBuild
+{
+    public abstract class DotNetMSBuild : RocketBoosterBuild
+    {
+        /// <summary>
+        /// Core target that can be used to trigger all targets for this build
+        /// </summary>
+        public Target MSBuild => _ => _;
+
+        /// <summary>
+        /// Use Coverage Collector instead of msbuild collector for code coverage
+        /// </summary>
+        [Parameter("Use Coverage Collector instead of msbuild collector for code coverage")]
+        public readonly bool CoverageCollector = false;
+
+        // /// <summary>
+        // /// This will ensure that all local dotnet tools are installed
+        // /// </summary>
+        // public Target DotnetToolRestore => _ => _
+        //    .DependsOn(Clean)
+        //    .DependentFor(DotNetCore)
+        //    .Unlisted()
+        //    .Executes(() => DotNet("tool restore"));
+
+        /// <summary>
+        /// dotnet restore
+        /// </summary>
+        public Target Restore => _ => _
+            .DependentFor(MSBuild)
+            // .DependsOn(DotnetToolRestore)
+            .DependsOn(Clean)
+            .Executes(() =>
+            {
+                DotNetRestore(s => s
+                    .SetProjectFile(Solution)
+                    .SetDisableParallel(true)
+                    .SetBinaryLogger(LogsDirectory / "restore.binlog", IsLocalBuild ? MSBuildBinaryLogImports.None : MSBuildBinaryLogImports.Embed)
+                    .SetFileLogger(LogsDirectory / "restore.log", Verbosity)
+                    .SetGitVersionEnvironment(GitVersion)
+                );
+            });
+
+        /// <summary>
+        /// dotnet build
+        /// </summary>
+        public Target Build => _ => _
+            .DependsOn(Restore)
+            .DependentFor(MSBuild)
+            .Executes(() =>
+            {
+                DotNetBuild(s => s
+                    .SetProjectFile(Solution)
+                    .SetBinaryLogger(LogsDirectory / "build.binlog", IsLocalBuild ? MSBuildBinaryLogImports.None : MSBuildBinaryLogImports.Embed)
+                    .SetFileLogger(LogsDirectory / "build.log", Verbosity)
+                    .SetGitVersionEnvironment(GitVersion)
+                    .SetConfiguration(Configuration)
+                    .EnableNoRestore());
+            });
+
+        /// <summary>
+        /// dotnet test
+        /// </summary>
+        public Target Test => _ => _
+            .DependsOn(Build)
+            .DependentFor(MSBuild)
+            .DependentFor(Pack)
+            .DependentFor(Generate_Code_Coverage_Reports)
+            .Triggers(Generate_Code_Coverage_Reports)
+            .OnlyWhenDynamic(() => TestDirectory.GlobFiles("**/*.csproj").Count > 0)
+            .WhenSkipped(DependencyBehavior.Execute)
+            .Executes(() =>
+            {
+                DotNetTest(s => {
+                    var a = s
+                        .SetProjectFile(Solution)
+                        .SetBinaryLogger(LogsDirectory / "test.binlog", IsLocalBuild ? MSBuildBinaryLogImports.None : MSBuildBinaryLogImports.Embed)
+                        .SetFileLogger(LogsDirectory / "test.log", Verbosity)
+                        .SetGitVersionEnvironment(GitVersion)
+                        .SetConfiguration(Configuration)
+                        .EnableNoRestore()
+                        .EnableNoBuild()
+                        .SetLogger($"trx")
+                        .SetProperty("CollectCoverage", !CoverageCollector)
+                        .SetProperty("CollectCoverage", true)
+                        .SetProperty("CoverageDirectory", CoverageDirectory)
+                        .SetProperty("IncludeDirectory", string.Join(", ", TestDirectory.GlobDirectories("**/bin").Select(x => (string)x)))
+                        .SetResultsDirectory(TestResultsDirectory);
+                    var b = (FileExists(TestDirectory / "coverlet.runsettings") ? a.SetSettingsFile(TestDirectory / "coverlet.runsettings") : a);
+                    return CoverageCollector ? b.SetDataCollector("XPlat Code Coverage") : b;
+                });
+                foreach (var coverage in TestResultsDirectory.GlobFiles("**/*.cobertura.xml"))
+                {
+                    CopyFileToDirectory(coverage, CoverageDirectory, FileExistsPolicy.OverwriteIfNewer);
+                }
+            });
+
+        /// <summary>
+        /// dotnet pack
+        /// </summary>
+        public Target Pack => _ => _
+            .DependsOn(Build)
+            .DependentFor(MSBuild)
+            .Executes(() =>
+            {
+                DotNetPack(s => s
+                    .SetProject(Solution)
+                    .SetBinaryLogger(LogsDirectory / "pack.binlog", IsLocalBuild ? MSBuildBinaryLogImports.None : MSBuildBinaryLogImports.Embed)
+                    .SetFileLogger(LogsDirectory / "pack.log", Verbosity)
+                    .SetGitVersionEnvironment(GitVersion)
+                    .SetConfiguration(Configuration)
+                    .EnableNoRestore()
+                    .SetOutputDirectory(NuGetPackageDirectory));
+            });
+    }
+}
